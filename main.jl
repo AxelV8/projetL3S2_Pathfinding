37391d79f0c56220bf1b00b53beeb95dfeef9fc0
Axel Verneuil <101639883+AxelV8@users.noreply.github.com>
2023-03-08 17:57:06 +0100

    modify main()

--- main.jl
+++ main.jl
@@ -1,9 +1,10 @@
 include("dijkstra.jl")
 include("convertion.jl")
 include("getImage.jl")
+include("a_star.jl")
 
 function main()
-map::Matrix{Char} = convertion_F_M("theglaive.map")
+map::Matrix{Char} = convertion_F_M("Expedition.map")
  
 
 #= weight of my character for [.,G,@,O,T,S,V] and create it =#
@@ -21,6 +22,7 @@ passage_Matrix::Matrix{Bool} = fill(false,size(map,1),size(map,2))
 pq = PriorityQueue{Tuple{Int64,Int64}, Int64}()
 
 
+
 #= Parent Matrix=#
 parent_Matrix::Matrix{Tuple{Int64,Int64}} = Matrix{Tuple{Int64,Int64}}(undef,size(map,1),size(map,2))
 
@@ -30,17 +32,26 @@ path::Vector{Tuple{Int64,Int64}} = []
 #= distance matrix=#
 distM::Matrix{Int64} = Matrix{Int64}(undef,size(map,1),size(map,2))
 
-#Pair of point=#
-s_deb::Tuple{Int64,Int64} = (50,250)
-s_fin::Tuple{Int64,Int64} = (400,400)
+#= acc that count how many nodes are traited=#
+nodes_traited::Int64 = 0
 
 
-dijkstra(map,passage_Matrix, s_deb, s_fin, pq, parent_Matrix, path, distM)
+#Pair of coordinate=#
+s_deb::Tuple{Int64,Int64} = (80,150)
+s_fin::Tuple{Int64,Int64} = (853,926)
+
+nodes_visitées::Vector{Tuple{Int64,Int64}} = []
+
+@time dijkstra(map,passage_Matrix, s_deb, s_fin, pq, parent_Matrix, path, distM, nodes_visitées) 
+
+#=@time a_star(map,passage_Matrix, s_deb, s_fin, pq, parent_Matrix, path, distM, nodes_visitées)=# 
+
+
 
 Image::Matrix{Vector{Int64}} = Matrix{Vector{Int64}}(undef,size(map,1),size(map,2))
 
 
-imshow(converte_M_I(map,Image,Color))
+imshow(converte_M_I(map,Image,Color,nodes_visitées,path))
 
 end
 
