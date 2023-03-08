8aa96d9f38a712c335d6a81561637574603d2c14
Axel Verneuil <101639883+AxelV8@users.noreply.github.com>
2023-03-08 17:53:11 +0100

    modify getImage
    Instead  of changing my char_Matrix, I just have two Vectors. I create my RVB Matrix I fill with the basic map represented by my char_Matrix. Then I colored all my open_nodes in the first Vector. Finally I colored the Path.

--- getImage.jl
+++ getImage.jl
@@ -7,19 +7,23 @@ Color::Dict{Char,Vector{Int64}} = Dict( '.' => [225,206,154],
 				    'W' => [16,52,166],
 				    'G' => [233,201,177],
 				    'O' => [0,0,0],
-				    'S' => [44,117,255],
-				    'X' => [172,30,68],
-				    'D' => [255,0,255],
-				    'F' => [255,255,0])
+				    'S' => [44,117,255])
 
 
-function converte_M_I(map::Matrix{Char}, Image::Matrix{Vector{Int64}}, Color::Dict{Char,Vector{Int64}})
-	
+function converte_M_I(map::Matrix{Char}, Image::Matrix{Vector{Int64}}, Color::Dict{Char,Vector{Int64}}, nodes_visitées::Vector{Tuple{Int64,Int64}}, path::Vector{Tuple{Int64,Int64}})
+	#= Changer avec les vecteurs  =#
 	for i in 1:size(map,1),j in 1:size(map,2)
 		
 		Image[i,j] = get!(Color, map[i,j], [255,255,255])
+		
 	end
-
+	for i in 1:length(nodes_visitées)
+		Image[(nodes_visitées[i])[1],(nodes_visitées[i])[2]] = [127,0,255]
+	 
+	end
+	for j in 1:length(path)
+		Image[(path[j])[1],(path[j])[2]] = [172,30,68]
+	end	
 return Image
 end
 	
