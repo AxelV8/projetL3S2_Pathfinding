268df3e3b7f47f748e5cd296735a00a4911e9db8
Axel Verneuil <101639883+AxelV8@users.noreply.github.com>
2023-03-08 17:43:11 +0100

    modify dijkstra
    Instead of modify my char_Matrix, i just put all my traited_nodes in a Vector of Tuple.

--- dijkstra.jl
+++ dijkstra.jl
@@ -37,17 +37,25 @@ function weight_transition(map::Matrix{Char}, c1::Tuple{Int64,Int64}, c2::Tuple{
 end
 
 #= take two nodes as a tuple and update the distance beetween may start_Node and my t2_Node =#
-function dist_update(t1::Tuple{Int64,Int64}, t2::Tuple{Int64,Int64}, distM::Matrix{Int64},pq, parent_Matrix::Matrix{Tuple{Int64,Int64}}, map::Matrix{Char})
+function dist_update(t1::Tuple{Int64,Int64}, t2::Tuple{Int64,Int64}, distM::Matrix{Int64},pq, parent_Matrix::Matrix{Tuple{Int64,Int64}}, map::Matrix{Char}, nodes_visitées::Vector{Tuple{Int64,Int64}})
 		
-
+	
 	if distM[t2[1],t2[2]] > distM[t1[1],t1[2]] + weight_transition(map,t1,t2)
 		
 		distM[t2[1],t2[2]] = distM[t1[1],t1[2]] + weight_transition(map,t1,t2)
 		
 		parent_Matrix[t2[1],t2[2]] = t1
-		#= enqueue! at the beginning but I change because when you have to put a Node again in the priority_queue with a lower distance, it fail. push replace the actual node and cahnge the priority=#
+	 	pushfirst!(nodes_visitées, t2)	
+		
+		#= enqueue! at the beginning but I change because when you have to put a Node again in the 
+		priority_queue with a lower distance, it fail. push replace the actual node and cahnge the priority=#
+		
+		
 		push!(pq,t2 => distM[t2[1],t2[2]])
+		
+		return 1
 	end
+	return 0
 end 
 
 #= Une fois que dijsktra est fini on a notre parent_matrice qui est good avec les distances on va chercher le plus court chemin cette fois avec la fun qui renvoi un vecteur de node =#
@@ -56,8 +64,7 @@ end
 	then I will create a graphic interface to Display my map with the path=#
 
 function min_path(path::Vector{Tuple{Int64,Int64}}, start_Node::Tuple{Int64,Int64}, end_Node::Tuple{Int64,Int64}, parent_Matrix::Matrix{Tuple{Int64,Int64}},map::Matrix{Char})
-	
-	map[end_Node[1],end_Node[2]] = 'F'	
+		
 
 	pushfirst!(path,end_Node)
 	node::Tuple{Int64,Int64} = parent_Matrix[end_Node[1],end_Node[2]]
@@ -69,11 +76,13 @@ function min_path(path::Vector{Tuple{Int64,Int64}}, start_Node::Tuple{Int64,Int6
 		
 		pushfirst!(path,node)
 		
-		map[node[1],node[2]] = 'X'
+		
+		#= don't change your char_Matrix =#
+		
 		
 		node = parent_Matrix[node[1],node[2]]
 	end
-	map[start_Node[1],start_Node[2]] = 'D' 
+	 
 	
 	pushfirst!(path,start_Node)
 	#=println(path)=#
@@ -82,13 +91,14 @@ function min_path(path::Vector{Tuple{Int64,Int64}}, start_Node::Tuple{Int64,Int6
 end
 
 #= Main function=#
-function dijkstra(map::Matrix{Char}, passage_Matrix::Matrix{Bool}, s_deb::Tuple{Int64,Int64}, s_fin::Tuple{Int64,Int64}, pq, parent_Matrix::Matrix{Tuple{Int64,Int64}}, path::Vector{Tuple{Int64,Int64}}, distM::Matrix{Int64} )
+function dijkstra(map::Matrix{Char}, passage_Matrix::Matrix{Bool}, s_deb::Tuple{Int64,Int64}, s_fin::Tuple{Int64,Int64}, pq, parent_Matrix::Matrix{Tuple{Int64,Int64}}, path::Vector{Tuple{Int64,Int64}}, distM::Matrix{Int64}, nodes_visitées::Vector{Tuple{Int64,Int64}}, nbEtat::Int64)
 
   distM::Matrix{Int64} = Initialisation(map,s_deb[1],s_deb[2])
 
   #=On utilise une priority_queue, on commence par mettre le node[x,y] avec priority 0 de debut dedans =#
 	
   enqueue!(pq,s_deb, distM[s_deb[1],s_deb[2]])
+  
  
   #=On commence la boucle:=#
   s1::Tuple{Int64,Int64} = (0,0)
@@ -97,6 +107,8 @@ function dijkstra(map::Matrix{Char}, passage_Matrix::Matrix{Bool}, s_deb::Tuple{
 
 	
 	s1 =  dequeue!(pq)
+		
+
 	if s1 == s_fin 
 		break
 	end
@@ -105,22 +117,34 @@ function dijkstra(map::Matrix{Char}, passage_Matrix::Matrix{Bool}, s_deb::Tuple{
 		 passage_Matrix[s1[1],s1[2]] = 1
 	
  	#=si le passage_matrix[s1[1],s1[2]] a son passage a false alors on le met a vrai=# 
-	
+	#= modif le robot n est pas tarzan: ajouter comme condition si la node est un Tree je le check pas =#	
     
-	if  (s1[1]+1 <= size(map,1) && map[s1[1]+1,s1[2]] != '@' && map[s1[1]+1,s1[2]] != 'O') 
-		dist_update(s1,(s1[1]+1,s1[2]) ,distM,pq, parent_Matrix, map)
+	if  (s1[1]+1 <= size(map,1) && map[s1[1]+1,s1[2]] != '@' && map[s1[1]+1,s1[2]] != 'O' 
+             && map[s1[1]+1,s1[2]] != 'T' && passage_Matrix[s1[1]+1,s1[2]] != 1) 
+
+		nbEtat += dist_update(s1,(s1[1]+1,s1[2]) , distM, pq, parent_Matrix, map, nodes_visitées)
+		
 	end
     
-	if  (s1[1]-1 > 0 && map[s1[1]-1,s1[2]] != '@' && map[s1[1]-1,s1[2]] != 'O')
-		dist_update(s1,(s1[1]-1,s1[2]),distM,pq, parent_Matrix, map)
+	if  (s1[1]-1 > 0 && map[s1[1]-1,s1[2]] != '@' && map[s1[1]-1,s1[2]] != 'O' && map[s1[1]-1,s1[2]] != 'T' 
+         && passage_Matrix[s1[1]-1,s1[2]] != 1)
+		
+        	nbEtat += dist_update(s1,(s1[1]-1,s1[2]), distM, pq, parent_Matrix, map, nodes_visitées)
+		
 	end
     
-    	if (s1[2]+1 <= size(map,2) && map[s1[1],s1[2]+1] != '@' && map[s1[1],s1[2]+1] != 'O') 
-		dist_update(s1,(s1[1],s1[2]+1), distM,pq, parent_Matrix, map)
+        if (s1[2]+1 <= size(map,2) && map[s1[1],s1[2]+1] != '@' && map[s1[1],s1[2]+1] != 'O'
+            && map[s1[1],s1[2]+1] != 'T' && passage_Matrix[s1[1],s1[2]+1] != 1) 
+		
+        	nbEtat += dist_update(s1,(s1[1],s1[2]+1), distM, pq, parent_Matrix, map, nodes_visitées)
+		
 	end
     
-    	if (s1[2]-1 > 0 && map[s1[1],s1[2]-1] != '@' && map[s1[1],s1[2]-1] != 'O')
-		dist_update(s1,(s1[1],s1[2]-1), distM,pq, parent_Matrix, map)
+    	if (s1[2]-1 > 0 && map[s1[1],s1[2]-1] != '@' && map[s1[1],s1[2]-1] != 'O' && map[s1[1],s1[2]-1] != 'T'
+            && passage_Matrix[s1[1],s1[2]-1] != 1)
+		
+        	nbEtat += dist_update(s1,(s1[1],s1[2]-1), distM,pq, parent_Matrix, map, nodes_visitées)
+		
 	end
 	#= checker aussi si les voisins sont des caracteres out of bound @ 0 T 
 		la on regarde pour ses voisins (x+1)< size(map,1) (x-1)>0 (y+1)<size(map,2) (y-1)>0, 
@@ -134,7 +158,9 @@ function dijkstra(map::Matrix{Char}, passage_Matrix::Matrix{Bool}, s_deb::Tuple{
 min_path(path, s_deb, s_fin, parent_Matrix,map)
 
 println("The lowest path that you should take is"," ", distM[s_fin[1],s_fin[2]])
-return parent_Matrix
+println(length(nodes_visitées)," "," nodes were traited during the algorithm")
+println(nbEtat," ", "Etat ouvert")
+return nodes_visitées
  
 end
 
