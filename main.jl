4bd0b778c70161d99f3270a49cf05535fcd40507
Axel Verneuil <101639883+AxelV8@users.noreply.github.com>
2023-03-10 18:48:56 +0100

    modify main()
    add arguments to the function

--- main.jl
+++ main.jl
@@ -3,8 +3,9 @@ include("convertion.jl")
 include("getImage.jl")
 include("a_star.jl")
 
-function main()
-map::Matrix{Char} = convertion_F_M("Expedition.map")
+function main(test::String)
+
+map::Matrix{Char} = convertion_F_M(test)
  
 
 #= weight of my character for [.,G,@,O,T,S,V] and create it =#
@@ -32,26 +33,34 @@ path::Vector{Tuple{Int64,Int64}} = []
 #= distance matrix=#
 distM::Matrix{Int64} = Matrix{Int64}(undef,size(map,1),size(map,2))
 
-#= acc that count how many nodes are traited=#
-nodes_traited::Int64 = 0
-
-
 #Pair of coordinate=#
 s_deb::Tuple{Int64,Int64} = (80,150)
 s_fin::Tuple{Int64,Int64} = (853,926)
 
 nodes_visitées::Vector{Tuple{Int64,Int64}} = []
 
-@time dijkstra(map,passage_Matrix, s_deb, s_fin, pq, parent_Matrix, path, distM, nodes_visitées) 
+println("Enter Dijkstra if you want to run Dijkstra or A_star if you wanna run a_star")
 
-#=@time a_star(map,passage_Matrix, s_deb, s_fin, pq, parent_Matrix, path, distM, nodes_visitées)=# 
+	algorithme_form = readline()
+	if algorithme_form == "D" 
+
+		@time dijkstra(map,passage_Matrix, s_deb, s_fin, pq, parent_Matrix, path, distM, nodes_visitées)
+   		 
+		Image_D::Matrix{Vector{Int64}} = Matrix{Vector{Int64}}(undef,size(map,1),size(map,2))
+		imshow(converte_M_I(map,Image_D,Color,nodes_visitées,path))
+		
+
+	elseif algorithme_form == "A"
+		@time a_star(map,passage_Matrix, s_deb, s_fin, pq, parent_Matrix, path, distM, nodes_visitées)
+
+		Image::Matrix{Vector{Int64}} = Matrix{Vector{Int64}}(undef,size(map,1),size(map,2))
+		imshow(converte_M_I(map,Image,Color,nodes_visitées,path))
+		
+	else 
+		error("re-run the main in your repl. Then, enter a valid expression please")	
+	end
 
 
-
-Image::Matrix{Vector{Int64}} = Matrix{Vector{Int64}}(undef,size(map,1),size(map,2))
-
-
-imshow(converte_M_I(map,Image,Color,nodes_visitées,path))
 
 end
 
