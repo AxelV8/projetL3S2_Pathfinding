b4a8a44a30ba06dd8feb6b5cb1c402d516498311
Axel Verneuil <101639883+AxelV8@users.noreply.github.com>
2023-03-12 15:22:44 +0100

    modify getImage
    change the loop because of my new Matrix of Bool  representing visited_nodes

--- getImage.jl
+++ getImage.jl
@@ -10,17 +10,19 @@ Color::Dict{Char,Vector{Int64}} = Dict( '.' => [225,206,154],
 				    'S' => [44,117,255])
 
 
-function converte_M_I(map::Matrix{Char}, Image::Matrix{Vector{Int64}}, Color::Dict{Char,Vector{Int64}}, nodes_visitées::Vector{Tuple{Int64,Int64}}, path::Vector{Tuple{Int64,Int64}})
+function converte_M_I(map::Matrix{Char}, Image::Matrix{Vector{Int64}}, Color::Dict{Char,Vector{Int64}}, nodes_visitées::Matrix{Bool}, path::Vector{Tuple{Int64,Int64}})
 	#= Changer avec les vecteurs  =#
 	for i in 1:size(map,1),j in 1:size(map,2)
-		
-		Image[i,j] = get!(Color, map[i,j], [255,255,255])
-		
+		if (nodes_visitées[i,j] == true)
+			Image[i,j] = [127,0,255]
+		else	
+			Image[i,j] = get!(Color, map[i,j], [255,255,255])
+		end
 	end
-	for i in 1:length(nodes_visitées)
+	#=for i in 1:length(nodes_visitées)
 		Image[(nodes_visitées[i])[1],(nodes_visitées[i])[2]] = [127,0,255]
 	 
-	end
+	end=#
 	for j in 1:length(path)
 		Image[(path[j])[1],(path[j])[2]] = [172,30,68]
 	end	
